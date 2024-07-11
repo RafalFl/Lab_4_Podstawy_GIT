4e97ae24e25ed5f98de994d60323a72554e0c584
Rafał <dsw54150@student.dsw.edu.pl>
2024-07-11 21:42:40 +0200

    Add --help flag to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,10 @@ if [ "$1" == "--logs" ]; then
     echo "log${i}.txt created by $0 on $(date)" > log${i}.txt
   done
 fi
+if [ "$1" == "--help" ]; then
+  echo "Available options:"
+  echo "--date   : Show current date"
+  echo "--logs   : Create 100 log files"
+  echo "--logs n : Create n log files"
+  echo "--help   : Show help"
+fi
