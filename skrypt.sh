716c865d3b98a31b50c9bf77407807f532b416af
Rafał <dsw54150@student.dsw.edu.pl>
2024-07-11 21:42:20 +0200

    Add argument support for --logs flag

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-if [ "$1" == "--date" ]; then
-  date
-fi
 if [ "$1" == "--logs" ]; then
-  for i in $(seq 1 100); do
+  num=${2:-100}
+  for i in $(seq 1 $num); do
     echo "log${i}.txt created by $0 on $(date)" > log${i}.txt
   done
 fi
